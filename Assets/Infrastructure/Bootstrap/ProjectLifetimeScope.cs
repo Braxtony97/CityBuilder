9631c4b22674c49b49c2ff398122e386f8b777d0
Renat <painren@mail.ru>
2025-12-01 15:37:34 +0300

    Added asmdef + create bootstrap

---
+++ Assets/Infrastructure/Bootstrap/ProjectLifetimeScope.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+public class ProjectLifetimeScope : LifetimeScope
+{
+    [SerializeField] private GameBootstrapper _bootstrapper;
+    [SerializeField] private CoroutineRunner _coroutineRunner;
+    
+    protected override void Configure(IContainerBuilder builder)
+    {
+        builder.RegisterInstance(_coroutineRunner).As<ICoroutineRunner>();
+        builder.RegisterInstance(_bootstrapper).As<IGameBootstrapper>();
+        builder.Register<SceneLoader>(resolver => 
+            new SceneLoader(resolver.Resolve<ICoroutineRunner>()), Lifetime.Singleton);
+    }
+
+    protected void Start()
+    {
+        Container.Inject(_bootstrapper);
+    }
+}
